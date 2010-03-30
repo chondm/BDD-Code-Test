cae2a7737ae8afc8d3f96afbc11fbca94bb8ff31
Chon Doan-Minh <domich.dt@gmail.com>
2010-03-30 16:23:51 +0700

    Start 2nd scenario

---
+++ app/controllers/features_controller.rb
@@ -0,0 +1,5 @@
+class FeaturesController < ApplicationController
+  def index
+    @features = Feature.all
+  end
+end
---
+++ features/step_definitions/gherkin_steps.rb
@@ -0,0 +1,8 @@
+Given /^there is feature titled "([^\"]*)"$/ do |title|
+  feature = Factory(:feature)
+end
+
+Given /^the feature has "([^\"]*)" of "([^\"]*)"$/ do |attribute, value|
+  pending # express the regexp above with the code you wish you had
+end
+
